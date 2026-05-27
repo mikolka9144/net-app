3edba3e30af10b1319865f5aab8ac3fbf6c9a2c5
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2026-05-27 12:03:55 +0200

    Update pipeline

---
+++ k8s/serviceaccount/make-kubefile.sh
@@ -0,0 +1,45 @@
+# The script returns a kubeconfig for the ServiceAccount given
+# you need to have kubectl on PATH with the context set to the cluster you want to create the config for
+
+# Cosmetics for the created config
+clusterName='some-cluster'
+# your server address goes here get it via `kubectl cluster-info`
+server='https://192.168.49.2:8443'
+# the Namespace and ServiceAccount name that is used for the config
+namespace='cloud'
+serviceAccount='github-robot'
+
+# The following automation does not work from Kubernetes 1.24 and up.
+# You might need to
+# define a Secret, reference the ServiceAccount there and set the secretName by hand!
+# See https://kubernetes.io/docs/tasks/configure-pod-container/configure-service-account/#manually-create-a-long-lived-api-token-for-a-serviceaccount for details
+secretName='github-robot-secret'
+
+######################
+# actual script starts
+set -o errexit
+
+
+ca=$(kubectl --namespace="$namespace" get secret/"$secretName" -o=jsonpath='{.data.ca\.crt}')
+token=$(kubectl --namespace="$namespace" get secret/"$secretName" -o=jsonpath='{.data.token}' | base64 --decode)
+
+echo "
+apiVersion: v1
+kind: Config
+clusters:
+  - name: ${clusterName}
+    cluster:
+      certificate-authority-data: ${ca}
+      server: ${server}
+contexts:
+  - name: ${serviceAccount}@${clusterName}
+    context:
+      cluster: ${clusterName}
+      namespace: ${namespace}
+      user: ${serviceAccount}
+users:
+  - name: ${serviceAccount}
+    user:
+      token: ${token}
+current-context: ${serviceAccount}@${clusterName}
+"
--- makefile
+++ makefile
@@ -17,6 +17,13 @@ helm-prod:
 	$(HELM) upgrade --install instance-backend ./helm/backend -f ./helm/backend/values.yaml -f ./helm/backend/values-prod.yaml
 	$(HELM) upgrade --install instance-frontend ./helm/frontend -f ./helm/frontend/values.yaml -f ./helm/frontend/values-prod.yaml
 
+cicd:
+	$(KUBECTL) config set-context --current --namespace=cloud
+	$(KUBECTL) create serviceaccount github-robot
+	$(KUBECTL) apply -f ./k8s/serviceaccount/role.yaml
+	$(KUBECTL) apply -f ./k8s/serviceaccount/rolebinding.yaml
+	$(KUBECTL) apply -f ./k8s/serviceaccount/secret.yaml
+
 k8s:
 	$(KUBECTL) apply -f ./k8s/cloud-namespace.yaml
 	$(KUBECTL) apply -f ./k8s/secrets/postgresql-credentials-dev.yaml
